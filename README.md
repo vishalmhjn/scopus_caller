e0cd3171d0f900e67201fa218fa224e1af07125e
Vishal <vishalmhjn3@gmail.com>
2020-04-05 18:18:11 +0200

    update and clean

--- README.md
+++ README.md
@@ -7,9 +7,10 @@
 Paste api_key from SCOPUS to input/API
 
 ## Using Keywords
-Create a file keywords.csv. The file should have two columns "First term" and "Second term". Then run ``python scrapper.py location_of_keywords.csv`` 
+Create a file keywords.csv. The file should have two columns "First term" and "Second term". Then run ``python keyword_scrapper.py location_of_keywords.csv`` 
 
 ## Unrestricted search
-``python apiCaller.py SEARCH_TERMS`` 
-e.g. ``python apiCaller.py transportation "road safety" "machine learning"``
+``python call_scopus.py SEARCH_TERMS`` 
+e.g. ``python call_scopus.py transportation "road safety" "machine learning"``
 - When a seacrh terms has a space ("machine learning"), use double quotations to enclose it (safety "machine learning")
+-for abstracts ``python call_semanticscholar.py path/to/acopus/results.csv output_filename``
--- src/call_semanticscholar.py
+++ src/call_semanticscholar.py
@@ -3,19 +3,28 @@ import requests
 import sys
 import time
 import pandas as pd
+from random import choice
 
-
-USER_AGENT = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) "
-			  "AppleWebKit/537.36 (KHTML, like Gecko) "
-			  "Chrome/54.0.2840.98 Safari/537.36"}
-
+desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
+ 
+def random_headers():
+    return {'User-Agent': choice(desktop_agents),'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
 def call_api(doi):
 
 	search_url = "http://api.semanticscholar.org/v1/paper/" + \
 		doi+"?include_unknown_references=true"
 
-	resp = requests.get(search_url, headers=USER_AGENT)
+	resp = requests.get(search_url, headers=random_headers())
 	content = resp.json()
 	return content
 
